4f05633ca62c6749dd696c5f85ab6ee231dd07f9
Puquii <RaivaxZM@gmail.com>
2024-01-14 16:21:51 -0300

    deleted folders

--- posts/forms.py
+++
@@ -1,7 +0,0 @@
-from django.forms import ModelForm
-from .models import Posts
-
-class CreatePost(ModelForm):
-    class Meta:
-        model = Posts
-        fields = ['title', 'link', 'og_price', 'price', 'description']
--- posts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('submit/', views.submit, name='submit'),
-    path('', views.index, name = 'index'),
-]
