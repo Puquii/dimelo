f9b751929f149c9ef9ab9e09d7214d9af0d0d414
Puquii <RaivaxZM@gmail.com>
2024-01-14 21:59:18 -0300

    ordered and renamed files

---
+++ posts/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from .forms import CreatePost
+from .models import Posts
+# Create your views here.
+
+def index(request):
+    discounts = Posts.objects.all().order_by('-created')
+    return render(request, 'index.html', {'discounts': discounts})
+
+def submit(request):
+    if request.method == 'POST':
+        form = CreatePost(request.POST)
+        if form.is_valid():
+            user_form = form.save(commit=False)
+            user_form.user = request.user
+            user_form.save()
+            return redirect('index')
+        
+    else:
+        form = CreatePost()
+    return render(request, 'posts/create_post.html', {'form': form})
