8daa25a26947e31d453f58cf065dcb5dbf2c4cca
Puquii <RaivaxZM@gmail.com>
2024-01-26 01:16:20 -0300

    added save func

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -10,9 +11,13 @@ class Posts(models.Model):
     title = models.CharField(max_length=200)
     og_price = models.IntegerField()
     price = models.IntegerField()
-    description = models.CharField(max_length=1000)
+    description = models.TextField(max_length=1000)
     user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-    slug = models.SlugField(default="", null=False)
+    slug = models.SlugField()
 
     def __str__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
