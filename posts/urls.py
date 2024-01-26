abdfcb2441d2bf1fb75df2506a81092763bba9bc
Puquii <RaivaxZM@gmail.com>
2024-01-26 01:17:30 -0300

    added url to edit a post

--- posts/urls.py
+++ posts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('submit/', views.submit, name='submit'),
     path('', views.index, name='index'),
-    path('description/<slug:slug>/', views.description, name='post_description')
+    path('description/<slug:slug>/', views.description, name='post_description'),
+    path('edit/<slug:slug>', views.edit, name='post_edit'),
 ]
